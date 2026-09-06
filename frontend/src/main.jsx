c329aa68f1ffdf32812ad6a6c926a7b6ecf7ffeb
Zakaria Shire <zshire2@uwo.ca>
2026-09-06 13:12:31 -0400

    Wrap app in ToastProvider and WorkspaceProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from "./hooks/UseAuth.jsx"
+import { WorkspaceProvider } from "./hooks/UseWorkspace.jsx"
+import { ToastProvider } from "./components/ui/Toast.jsx"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <ToastProvider>
+      <WorkspaceProvider>
+        <App />
+      </WorkspaceProvider>
+    </ToastProvider>
   </AuthProvider>
 )
